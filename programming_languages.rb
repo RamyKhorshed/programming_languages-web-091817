4f07faed9129136be662754b57e442b92b2afefa
Ramy Khorshed <RamyKhorshed@gmail.com>
2017-09-19 16:25:00 -0400

    completed lab, fixed array that was overwriting

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,57 @@
+require 'pry'
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
   # your code here
+  # Make a method finding object oriented languages + functional languages
+  # add :style key = oo or functional
+  hash = {}
+  languagesArray =[]
+  languages.each do |key,typesData|
+    typesData.each do |title, info|
+      if hash[title]
+        hash[title][:style].push(key)
+      else
+        hash[title] = info
+        hash[title][:style] = []
+        hash[title][:style].push(key)
+      end
+      # binding.pry
+    end
+  end
+  # binding.pry
+  hash
 end
+
+# languagesArray = reformat_languages(languages)
